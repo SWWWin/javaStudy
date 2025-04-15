6c047f42f45c2ed60672c6372d9195e1cd45de94
SWWWin <bgpd1258@naver.com>
2025-04-15 23:53:39 +0900

    feat: String 클래스(기본, 비교, 불변 객체, 주요 메서드, 가변 String)학습 정리

---
+++ untitled/src/lang/string/CharArrayMain.java
@@ -0,0 +1,11 @@
+package lang.string;
+
+public class CharArrayMain {
+    public static void main(String[] args) {
+        char[] charrArr = new char[]{'h', 'e', 'l', 'l', 'o'};
+        System.out.println(charrArr);
+
+        String str= "hello";
+        System.out.println("str = " + str);
+    }
+}
